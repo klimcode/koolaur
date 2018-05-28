1a8937cba0918a420e5fb0427d97695e7c4d208b
Evgenii Klimov <disturh@gmail.com>
2018-05-28 12:29:07 +0300

    New Illustrations

--- src/commonBundle/pages/Main.js
+++ src/commonBundle/pages/Main.js
@@ -3,23 +3,33 @@ import React from 'react';
 import Gallery from '../templates/Gallery';
 import { convertFilesToProjects } from '../atoms/functions';
 
-// Crashes old mobile Safari !!!
+// Loads images automatically but Crashes old mobile Safari !!!
 // const images = require.context('../../../public/static/media', false, /\.(jpe?g|png)$/).keys();
 
 const files = [
-  { name: 'Annihilation', thumbSrc: 'annihilation-min.jpg' },
+  { name: 'The princess of Tea Ceremonies', thumbSrc: 'princess-tea-min.jpg' },
+  { name: 'The Prince who Grows Moss', thumbSrc: 'prince-moss-min.jpg' },
+  { name: 'The princess of Beetles and Butterflies', thumbSrc: 'princess-beetles-min.jpg' },
+  { name: 'The Princess of Cozy Yarn', thumbSrc: 'princess-yarn-min.jpg' },
+  { name: 'The Lantern Guardian Princess', thumbSrc: 'princess-lantern-min.jpg' },
+  { name: 'Princess of Soft Blankets', thumbSrc: 'princess-blankets-min.jpg' },
   { name: 'A quiet place', thumbSrc: 'a-quiet-place-min.jpg' },
+  { name: 'Annihilation', thumbSrc: 'annihilation-min.jpg' },
   { name: 'A witch', thumbSrc: 'witch-min.jpg' },
   { name: 'Mary and a Witch\'s flower', thumbSrc: 'mary-min.jpg' },
   { name: 'Koolaur', thumbSrc: 'koolaur-min.jpg' },
   { name: 'Sea', thumbSrc: 'sea-min.jpg' },
-  { name: 'Sweet river', thumbSrc: 'sweet-rivers-min.jpg' },
   { name: 'A Parisian naturalist', thumbSrc: 'naturalist-min.jpg' },
+  { name: 'Happy Christmas', thumbSrc: 'happy-cristmas-min.jpg' },
   { name: 'Must be a Weasley', thumbSrc: 'weasley-min.jpg' },
+  { name: 'Luna Lovegood', thumbSrc: 'luna-min.jpg' },
+  { name: 'A Harry Potter movie', thumbSrc: 'hp-min.jpg' },
+  { name: 'Music', thumbSrc: 'music-min.jpg' },
+  { name: 'Video games', thumbSrc: 'video-games-min.jpg' },
+  { name: 'Sweet river', thumbSrc: 'sweet-rivers-min.jpg' },
   { name: 'A secret', thumbSrc: 'a-secret-min.jpg' },
   { name: 'Festive forest', thumbSrc: 'festive-forest-min.jpg' },
-  { name: 'Happy Christmas', thumbSrc: 'happy-cristmas-min.jpg' },
-  { name: 'A Harry Potter movie', thumbSrc: 'hp-min.jpg' },
+  { name: 'Sunrise', thumbSrc: 'sunrise-min.jpg' },
   {
     name: 'Letters vol. 1',
     thumbSrc: 'letters-1-min.jpg',
@@ -30,11 +40,7 @@ const files = [
     thumbSrc: 'letters-2-min.jpg',
     originSrc: ['letters/l-2-01.jpg', 'letters/l-2-02.jpg', 'letters/l-2-03.jpg', 'letters/l-2-04.jpg', 'letters/l-2-05.jpg', 'letters/l-2-06.jpg', 'letters/l-2-07.jpg', 'letters/l-2-08.jpg'],
   },
-  { name: 'Luna Lovegood', thumbSrc: 'luna-min.jpg' },
-  { name: 'Music', thumbSrc: 'music-min.jpg' },
   { name: 'Sky may fall', thumbSrc: 'sky-min.jpg' },
-  { name: 'Sunrise', thumbSrc: 'sunrise-min.jpg' },
-  { name: 'Video games', thumbSrc: 'video-games-min.jpg' },
   // { name: '', thumbSrc: '' },
 ];
 const urlBase = '/';
